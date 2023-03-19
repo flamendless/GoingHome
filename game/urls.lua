5ace1840dd05994a8de16cbe225e227688f102be
flamendless <flamendless8@gmail.com>
2023-03-19 20:55:25 +0800

    Update urls

--- game/urls.lua
+++ game/urls.lua
@@ -1,9 +1,8 @@
 local URLS = {
-	twitter = "https://twitter.com/flamendless",
+	twitter = "https://twitter.com/flam8studio",
 	paypal = "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=K7QQSFGC2WXEA",
 	mailto = "mailto:flamendless8@gmail.com",
-	--TODO: (Brandon) replace this with itch.io
-	game_page = "https://brbl.gamejolt.io"
+	game_page = "https://flamendless.itch.io/going-home-revisited"
 }
 
 return URLS
