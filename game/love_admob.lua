ffe406ad94e259ae843182a13f001a25f8a4d517
flamendless <flamendless8@gmail.com>
2023-12-14 09:33:57 +0800

    Remove debugging prints

--- game/love_admob.lua
+++ game/love_admob.lua
@@ -5,8 +5,8 @@ love_admob.updateTime = 1 --Seconds
 
 function love_admob.update(dt)
 	if love_admob.timer > 1 then
-		print(Inspect(love_admob))
-		print("Test", love_admob.test())
+		-- print(Inspect(love_admob))
+		-- print("Test", love_admob.test())
 		love_admob.checkForAdsCallbacks()
 		love_admob.timer = 0
 	end
@@ -14,29 +14,24 @@ function love_admob.update(dt)
 end
 
 function love_admob.checkForAdsCallbacks()
-	print(2, love_admob.coreInterstitialError)
-
 	if love_admob.coreInterstitialError() then --Interstitial failed to load
 		if love_admob.interstitialFailedToLoad then
 			love_admob.interstitialFailedToLoad()
 		end
 	end
 
-	print(3, love_admob.coreInterstitialClosed)
 	if love_admob.coreInterstitialClosed() then --User has closed the ad
 		if love_admob.interstitialClosed then
 			love_admob.interstitialClosed()
 		end
 	end
 
-	print(4, love_admob.coreRewardedAdError)
 	if love_admob.coreRewardedAdError() then --Rewarded ad failed to load
 		if love_admob.rewardedAdFailedToLoad then
 			love_admob.rewardedAdFailedToLoad()
 		end
 	end
 
-	print(5, love_admob.coreRewardedAdDidFinish)
 	if love_admob.coreRewardedAdDidFinish() then --Video has finished playing
 		local rewardType = "???"
 		local rewardQuantity = 1
@@ -48,7 +43,6 @@ function love_admob.checkForAdsCallbacks()
 		end
 	end
 
-	print(6, love_admob.coreRewardedAdDidStop)
 	if love_admob.coreRewardedAdDidStop() then --Video has stopped by user
 		if love_admob.rewardedAdDidStop then
 			love_admob.rewardedAdDidStop()
