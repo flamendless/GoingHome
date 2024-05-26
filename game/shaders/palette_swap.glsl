a119daa8b694401808b4449bb73cce88f7167456
Maxim Gorin <onlinexmax@gmail.com>
2024-05-26 19:49:40 +0300

    Fix shader for gles

--- game/shaders/palette_swap.glsl
+++ game/shaders/palette_swap.glsl
@@ -4,7 +4,7 @@ vec4 effect( vec4 color, Image tex, vec2 texture_coords, vec2 screen_coords )
 {
     vec4 index = Texel(tex, texture_coords);
 
-    if (index.a == 0)
+    if (index.a == 0.0)
       discard;
 
     vec4 pixel = Texel(u_tex_palette, vec2(index.x, 1));
